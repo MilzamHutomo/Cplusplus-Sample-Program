d62d43c2a4e73ac4be0b2c0fc612d4d2d8bf5bb1
Milzam Hutomo <37233788+MilzamHutomo@users.noreply.github.com>
2019-01-30 10:20:21 +0700

    Update Program Penghitung Bilangan Faktorial.cpp

--- Program Penghitung Bilangan Faktorial.cpp
+++ Program Penghitung Bilangan Faktorial.cpp
@@ -2,14 +2,25 @@
 
 using namespace std;
 
+/**
+ * Input validation since factorial only for positive number
+ **/
 bool validation(int num);
+
+/**
+ * sub-program to show "X! = A x B x C = RESULT"
+ **/
 void factorial(int num);
+
+/**
+ * Function to count the result of its factorial
+ **/
 int count(int num);
 
 int main(){
     int num;
 
-    cout << "Masukkan Angka: "; cin >> num;
+    cout << "Input Number: "; cin >> num;
 
     if(!validation(num)){
         factorial(num);
@@ -18,7 +29,7 @@ int main(){
 
 bool validation(int num){
     if(num < 0){
-        cout << "Input tidak valid" << endl;
+        cout << "Input not valid" << endl;
         return 1;
     }else if(num == 0){
         cout << "0! = 1" << endl;
@@ -45,4 +56,5 @@ int count(int num){
     for(int i=num; i>0; i--){
         counter = counter*i;
     }
+    return counter;
 }
